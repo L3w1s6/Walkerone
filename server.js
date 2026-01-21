9f46050453936734980693844f4edb2bfd3991a3
godfrey-ck-lam <godfreycklam@gmail.com>
2026-01-21 13:05:23 +0000

    fixed backend

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 
 import express from "express";
 import mongoose from "mongoose";
+import "dotenv/config";
+
+const URI = process.env.URI
+const PORT = process.env.PORT
 const app = express();
 
-const PORT = 8000;
+async function connect() {
+    try {
+        await mongoose.connect(URI)
+        console.log("connected to server")
+    } catch (error) {
+        console.log(error)
+    }
+}
 
+connect()
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
