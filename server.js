95d928bfd30c4c66c4ef436f1c35613354c6a032
godfrey-ck-lam <godfreycklam@gmail.com>
2026-01-21 12:50:36 +0000

    Revert "fixed mongodb connection"
    This reverts commit 329255b4ce26a2da441158c33712a23ac68bd28c.

--- server.js
+++ server.js
@@ -5,16 +5,4 @@ const app = express();
 
 const PORT = 8000;
 
-const uri = "mongodb+srv://WalkeroneDev:uAlVnEwVFDa5G4EX@walkerone.hqlvq8e.mongodb.net/?appName=Walkerone"
-async function connect(){
-    try {
-    await mongoose.connect(uri)
-    console.log("successfully connected to mongoDB")
-    }catch(error){
-        console.log(error)
-    }
-
-}
-
-connect();
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
