36320a85d19df60ac8e31c9904c75825c93a518d
Thomas Fraser <8thomas0fraser8@gmail.com>
2026-01-21 13:46:15 +0000

    added working links to top nav

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,17 +1,30 @@
-import BottomNav from "./components/BottomNav"
-import MobileBody from "./components/MobileBody"
-import Test from "./components/test"
-import TopNav from "./components/TopNav"
+import MobileBody from './components/MobileBody'
+import TopNav from './components/TopNav'
+import BottomNav from './components/BottomNav'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Test from './components/test'
+import Account from './pages/Account'
+import Login from './pages/Login'
+import Map from './pages/Map'
+import Stats from './pages/Stats'
+import Tasks from './pages/Tasks'
 
 export default function App() {
   return (
-    <MobileBody>
-      <TopNav/>
-      <div className='text-center'>
-        <Test />
-        <p className="text-3xl">test</p>
-      </div>
-      <BottomNav/>
-    </MobileBody>
+    <BrowserRouter>
+      <MobileBody>
+        <TopNav />
+        <Routes>
+          <Route path="/" element={<Test />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/map" element={<Map />} />
+          <Route path="/routes" element={<Routes />} />
+          <Route path="/stats" element={<Stats />} />
+          <Route path="/tasks" element={<Tasks />} />
+        </Routes>
+        <BottomNav />
+      </MobileBody>
+    </BrowserRouter>
   )
 }
--- Frontend/src/components/TopNav.jsx
+++ Frontend/src/components/TopNav.jsx
@@ -1,20 +1,23 @@
+import { Link } from 'react-router-dom'
 import {HiOutlineUserCircle, HiChevronDown} from "react-icons/hi";
 
-const Item = ({name, icon: Icon}) => {
+const Item = ({name, icon: Icon, path}) => {
     return (
-        <div className="flex flex-col items-center text-gray-600 cursor-pointer">
-            <span className="text-xl font-medium mb-1">{name}</span>
-            <Icon className="w-12 h-12 text-gray-300"/>
-        </div>
+        <Link to={path}>
+            <div className="flex flex-col items-center text-gray-600 cursor-pointer">
+                <span className="text-xl font-medium mb-1">{name}</span>
+                <Icon className="w-12 h-12 text-gray-300"/>
+            </div>
+        </Link>
     )
 };
 
 export default function TopNav() {
     return (
         <div className="bg-cyan-50 pt-4 pb-4 px-4 flex justify-evenly items-center z-10">
-            <Item name="Goals" icon={HiChevronDown}/>
-            <Item name="Stats" icon={HiChevronDown}/>
-            <Item name="Account" icon={HiOutlineUserCircle}/>
+            <Item name="Goals" icon={HiChevronDown} path="/tasks"/>
+            <Item name="Stats" icon={HiChevronDown} path="/stats"/>
+            <Item name="Account" icon={HiOutlineUserCircle} path="/account"/>
         </div>
     )
 };
