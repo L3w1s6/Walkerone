598950275a691bf9d45cb75a0498254fc3b43e07
Thomas Fraser <8thomas0fraser8@gmail.com>
2026-01-21 11:44:16 +0000

    - (hopefully) setup react with vite and tailwind

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Test from './components/test'
+
+function App() {
+
+  return (
+    <>
+    <p className="text-3xl">test</p>
+    <Test />
+    </>
+  )
+}
+
+export default App
