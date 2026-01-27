64d9e73ce739b339522c90ba491f68e6a8be9ca3
godfrey-ck-lam <godfreycklam@gmail.com>
2026-01-27 16:22:28 +0000

    tested backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,13 @@
 import express from "express";
 import mongoose from "mongoose";
+import mongodb, { MongoClient } from "mongodb";
 import "dotenv/config";
 
 const URI = process.env.URI
 const PORT = process.env.PORT
 const app = express();
+const client = new MongoClient(URI);
+
 
 async function connect() {
     try {
@@ -15,5 +18,29 @@ async function connect() {
     }
 }
 
+async function run() {
+
+try {
+
+await client.connect();
+
+// the database itself (Walkerone)
+const db = client.db('sample_mflix');
+
+// the table you want to query (users, routes)
+const collection = db.collection('movies');
+
+// get the first value 
+const first = await collection.findOne();
+
+console.log(first);
+
+} finally {
+     await client.close();
+}
+
+}
+
 connect();
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+run().catch(console.error);
