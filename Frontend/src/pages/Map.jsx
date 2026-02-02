0aba6d8e4901003cc66a9a4a1da71a30285b39d3
Lewis Turner <lewis.turner.2016@gmail.com>
2026-02-02 11:16:40 +0000

    added error catching for Mapbox

--- Frontend/src/pages/Map.jsx
+++ Frontend/src/pages/Map.jsx
@@ -1,23 +1,55 @@
-import { useRef, useEffect } from 'react'
+import {useRef, useEffect, useState} from 'react'
 import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css';
 
 export default function Map() {
-    const mapRef = useRef()
-    const mapContainerRef = useRef()
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+  const [error, errorFun] = useState(null)//error state val, function to update it
 
-    useEffect(() => {
-    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_TOKEN
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-    });
+  useEffect(() => {
+    //check browser support
+    if (!mapboxgl.supported()) {
+      errorFun("Browser doesn't support Mapbox GL")
+      return
+    }
+
+    //attempt to load map within try catch instead of breaking entire site
+    try {
+      //check for API token
+      if (!mapboxgl.accessToken) {
+        throw new Error("Missing Mapbox API token")
+      }
+
+      mapRef.current = new mapboxgl.Map({
+        container: mapContainerRef.current
+      })
+
+      //async internal Mapbox errors
+      mapRef.current.on("error", (e) => {
+        console.error("Map internal error: ", e)
+        errorFun("Failed to load map, internal error: " + e.message)
+      })
+    } catch (e) {//initialising errors
+      console.error("Map error: ", e)
+      errorFun("Failed to load map: " + e.message)
+    }
 
     return () => {
-      mapRef.current.remove()
+      if (mapRef.current) {
+        mapRef.current.remove()
+      }
     }
   }, [])
 
-    return(
-      <div id='map-container' ref={mapContainerRef} style={{height: '100%', width: '100%'}}/>
+  //display <p> if error exists
+  if (error) {
+    return (
+      <div><p className="text-red-600">{error}</p></div>
     )
+  }
+
+  return(
+    <div id='map-container' ref={mapContainerRef} style={{height: '100%', width: '100%'}}/>
+  )
 }
