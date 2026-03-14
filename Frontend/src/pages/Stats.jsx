3ac3f1103dee963d5e120189e74135105e2e318c
Lewis <lewis.turner.2016@gmail.com>
2026-03-14 01:29:04 +0000

    in progress stats fetching stuff

--- Frontend/src/pages/Stats.jsx
+++ Frontend/src/pages/Stats.jsx
@@ -12,9 +12,6 @@ import {
     RadialLinearScale,
 } from 'chart.js';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
-
-import {HiRewind, HiFastForward} from "react-icons/hi";
-
 //required to explicitly register used components
 ChartJS.register(
     CategoryScale,
@@ -28,6 +25,9 @@ ChartJS.register(
     RadialLinearScale,
 );
 
+import {HiRewind, HiFastForward} from 'react-icons/hi';
+import {useState, useEffect} from 'react';
+
 //function to create heartrate colour gradient
 function getGrad(context) {
     let {ctx, chartArea} = context.chart
@@ -37,19 +37,34 @@ function getGrad(context) {
     return grad
 }
 
-const DateBtn = ({icon: Icon}) => {
+const DateBtn = ({icon: Icon, click}) => {
     return (
-        <div className="w-20 rounded-lg flex flex-col items-center text-gray-600 cursor-pointer transition hover:scale-115">
+        <button className="w-20 rounded-lg flex flex-col items-center text-gray-600 cursor-pointer transition hover:scale-115" onClick={click}>
             <Icon className="w-12 h-12 text-green-300"/>
-        </div>
+        </button>
     )
 };
 
+//helper funs for converting dates to strings
+function dateFormatted(date) {return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear() % 100}`;}
+
+function dateRangeStr(range) {return dateFormatted(range.start) + " - " + dateFormatted(range.end);}
+
+//date range
+var weekRange = {
+    start: Date,
+    end: Date
+}
+//init range to current week
+const today = new Date();
+weekRange.start = new Date(today.getFullYear(), today.getMonth(), today.getDate() - (today.getDay() - 1) - 1)
+weekRange.end = new Date(today.getFullYear(), today.getMonth(), today.getDate() + (7 - (today.getDay() - 1)) - 1)
+
 export default function Stats() {
-    //Heartrate chart
-    //manual data & labels for now
-    const heartLabels = [1,2,3,4,5,6,7,8,9,10]
-    const heartData = [
+    const email = localStorage.getItem("userEmail");
+
+    const [weekStr, setWeekStr] = useState(dateRangeStr(weekRange));//display date range
+    const [heartData, setHeartData] = useState([
         {x: 1, y: 100},
         {x: 2, y: 120},
         {x: 3, y: 80},
@@ -58,7 +73,13 @@ export default function Stats() {
         {x: 6, y: 120},
         {x: 7, y: 60},
         {x: 8, y: 70},
-    ]
+    ]);//heartrate data (points)
+    const [stepData, setStepData] = useState([7500, 2500]);//steps data (taken, remaining)
+    const [calData, setCalData] = useState([1500, 1500]);//calories data (burned, remaining)
+
+    //Heartrate chart
+    //manual data & labels for now
+    const heartLabels = [1,2,3,4,5,6,7,8,9,10]
     const heartDatasets = {
         labels: heartLabels,
         datasets: [
@@ -114,7 +135,6 @@ export default function Stats() {
     }
 
     //Steps chart
-    const stepData = [7500,2500]
     const stepDatasets = {
         labels: ["Done","Remaining"],
         datasets: [
@@ -151,7 +171,6 @@ export default function Stats() {
     }
 
     //Calories chart
-    const calData = [1500,1500]
     const calDatasets = {
         labels: ["Burned","Remaining"],
         datasets: [
@@ -187,6 +206,32 @@ export default function Stats() {
         }
     }
 
+    //onClick fun to move date range
+    const dateShift = async(diff) => {
+        weekRange.start.setDate(weekRange.start.getDate() + diff);
+        weekRange.end.setDate(weekRange.end.getDate() + diff);
+        setWeekStr(dateRangeStr(weekRange));
+        getPeriodData();
+        console.log(weekRange);
+    }
+
+    //Get routes within date range & convert to charts
+    const getPeriodData = async() => {
+        try {
+            const response = await fetch(`/getRoutesPeriod?email=${email}&offset=${7}`);
+            if (!response.ok) {
+                console.log("not found");
+                return;
+            }
+            console.log(response);
+
+            const data = await response.json();//always empty for some reason
+            console.log("Data: ", data);
+        } catch (e) {
+            console.error("Period fetch error: ", e);
+        }
+    }
+
     return (
         <div className="flex flex-col w-full h-full justify-around gap-4 p-2">
             <div className="flex flex-col w-full gap-2 text-center mt-2"> {/* Title */}
@@ -207,9 +252,9 @@ export default function Stats() {
             </div>
 
             <div className="flex justify-between items-center w-full pb-2"> {/* Date Incrementer */}
-                <DateBtn icon={HiRewind} />
-                <p className="text-2xl font-bold text-gray-600">12/3/26</p>
-                <DateBtn icon={HiFastForward} />
+                <DateBtn icon={HiRewind} click={() => dateShift(-7)}/>
+                <p className="text-2xl font-bold text-gray-600">{weekStr}</p>
+                <DateBtn icon={HiFastForward} click={() => dateShift(7)}/>
             </div>
         </div>
     )
